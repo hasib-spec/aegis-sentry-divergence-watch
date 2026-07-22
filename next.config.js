73f91d9cf734d9fbfbe5ab7c3c97837973c1cabe
hasib-spec <haedu59@gmail.com>
2026-07-23 00:51:07 +0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -6,6 +6,30 @@ const nextConfig = {
     config.module.exprContextCritical = false;
     return config;
   },
+  headers: async () => [
+    {
+      source: "/sw.js",
+      headers: [
+        {
+          key: "Cache-Control",
+          value: "public, max-age=0, must-revalidate",
+        },
+        {
+          key: "Service-Worker-Allowed",
+          value: "/",
+        },
+      ],
+    },
+    {
+      source: "/manifest.json",
+      headers: [
+        {
+          key: "Cache-Control",
+          value: "public, max-age=600",
+        },
+      ],
+    },
+  ],
 };
 
 module.exports = nextConfig;
